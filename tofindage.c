c49bc82cf8a0e40e03e468d54f6313bac69c4ec7
kathanzula <147814193+kathanzula@users.noreply.github.com>
2024-02-02 00:07:29 +0530

    Create tofindage.c

---
+++ tofindage.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void){
+int x,y;
+printf("what year you were born in =");
+scanf("%d",&x);
+printf("what is current year = ");
+scanf("%d",&y);
+int age = y-x+1;
+printf("your current age is = %d",age);
+return 0;
+}
