ba6ceb394dc70b52de5ead5ca2d060b3764cc71d
kathanzula <147814193+kathanzula@users.noreply.github.com>
2024-02-02 01:35:47 +0530

    Create tofindprime.c

---
+++ tofindprime.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    if(a==1 || a==0){
+        printf("the number is niether prime nor composite");
+    }else{
+        for(int i =2;i<a;i++){
+            if(a%i==0){
+                printf("not prime");
+                break;
+            }else{
+                printf("prime");
+            
+                break;
+            }
+        }
+    }
+}
